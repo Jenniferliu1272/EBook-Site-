61ff1d1be93dd2dc58cfa63af33bfb44a8394308
Jen Lamere <jennielamere@gmail.com>
2016-10-24 12:20:33 -0400

    adds a genre browser view

--- bookSell/views.py
+++ bookSell/views.py
@@ -14,8 +14,11 @@ def index(request):
     top_books = Book.objects.order_by('-rating')[:5]
     new_books = Book.objects.order_by('-year_published')[:5]
 
+    # books for the genre selector
+    genre = request.GET.get('genre') if request.GET.get('genre') is not None else 0
+    genre_books = Book.objects.filter(genre=genre).order_by('-rating')[:4]
     genres = ['Science fiction','Drama', 'Action and Adventure','Romance','Mystery','Health','Children\'s','Science','History','Biographies']
-    return render(request, 'books/homepage/index.html', {'top_books': top_books, 'new_books': new_books, 'genres' : genres})
+    return render(request, 'books/homepage/index.html', {'top_books': top_books, 'new_books': new_books, 'book_by_genre' : genre_books, 'genres' : genres, 'genre': genres[int(genre)]})
 
 
 headers = {'cost':'asc',
