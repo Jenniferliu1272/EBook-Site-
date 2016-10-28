6b64f523290b4a9a86974618fc4be7f3d85099ba
Jen Lamere <jennielamere@gmail.com>
2016-10-28 15:35:59 -0400

    scaffolds genre page a bit

--- bookSell/views/general_views.py
+++ bookSell/views/general_views.py
@@ -27,38 +27,11 @@ def search(request):
     return render(request, 'books/search/search_results.html', {'query':query, 'books':results, 'genres' : genres})
 
 def genre(request, genre):
-    return render(request, 'books/genre_page/genre_page.html', {'genre': genre, 'genres' : genres})
+    books = Book.objects.filter(genre=genres.index(genre)).order_by('rating')
+    top_book = books[0]
+    new_book = Book.objects.filter(genre=genres.index(genre)).order_by('year_published')[0]
+    return render(request, 'books/browse_genre/genre.html', {'genre': genre, 'genres' : genres, 'books': books, 'top_book':top_book, 'new_book': new_book})
 
 def condition_view(request):
     canView = True
     return render(request, 'books/individual_book/book_view.html', )
-
-def sell_book_view_p1(request, book_id):
-    sellform = sell_book_form_p1()
-    status = False
-    if request.method == 'POST':
-        sellForm = sell_book_form_p1(request.POST)
-        if sellForm.is_valid():
-            new_book = sellForm.save_sell_form_p1()
-            status = True
-            selfForm2 = sell_book_form_p2()
-            return render(request, "books/sell_buy_form/sellForm.html", {'status': status, 'genres' : genres, 'new_book': new_book, 'selfForm2': selfForm2 })
-        else:
-            status = False
-            return render(request, "books/sell_buy_form/sellForm.html", {'status': status, 'genres' : genres})
-    else:
-        return render(request, "books/sell_buy_form/sellForm.html", {'sellform': sellform, 'genres' : genres, 'status': status})
-
-def sell_book_view_p2(request, book_id):
-    status2 = False
-    if request.method == 'POST':
-        sellForm2 = sell_book_form_p2(request.POST, instance=Book.objects.get(id=book_id))
-        if sellForm2.is_valid():
-            current_book = sellForm2.save_sell_form_p2(book_id=book_id)
-            status2 = True
-            return render(request, "books/sell_buy_form/confirm.html", {'status': status2, 'genres' : genres, 'current_book': current_book})
-        else:
-            status2 = False
-            return render(request, "books/sell_buy_form/sellForm.html", {'status': status2, 'genres' : genres})
-    else:
-        return render(request, "books/sell_buy_form/sellForm.html", {'sellform2': sell_book_form_p2, 'genres' : genres, 'status': status2})
