decbfc1f410a4a74329ffa3aa60516c543baae59
JenniferLiu <jwt7689@rit.edu>
2016-11-23 00:48:02 -0500

    Finished Buy

--- bookSell/admin.py
+++ bookSell/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
 admin.site.register(Book)
 admin.site.register(BookForSale)
 admin.site.register(UserProfile)
 admin.site.register(BookRating)
+admin.site.register(Payment, PaymentAdmin)
+
+
