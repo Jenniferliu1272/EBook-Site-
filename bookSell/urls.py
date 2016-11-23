c94b998f741c1b5092fdfd04cda1620199c32383
JenniferLiu <jwt7689@rit.edu>
2016-11-23 00:48:32 -0500

    Finished Buy

--- bookSell/urls.py
+++ bookSell/urls.py
@@ -39,6 +39,9 @@ urlpatterns = [
 	url(r'^sell_results/', views.sell_search, name='sell_search'),
 	url(r'sell_existing/(?P<book_id>[0-9]+)/(?P<back>.*)?', views.sell_existing, name="sell_existing"),
 	url(r'^sell_original/', views.sell_original, name='sell_original'),
-]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+	#Buy Urls
+	url(r'^buy/(?P<book_id>[0-9]+)', views.buy_book, name='buy_book'),
+]  +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
