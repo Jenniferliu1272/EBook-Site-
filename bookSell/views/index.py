cdd9115a42b9e10f40dc1f2a4b34accd6e740550
Ram Longman <Ram Longman>
2016-11-25 14:38:08 -0500

    recommendations-incomplete, need to finish the query

--- bookSell/views/index.py
+++ bookSell/views/index.py
@@ -6,12 +6,13 @@ from bookSell.models import Book
 from bookSell.constants import genres
 
 def index(request):
-    # two modules being displayed on top of page
-    top_books = Book.objects.order_by('-year_published')[:5]
-    new_books = Book.objects.order_by('-year_published')[:5]
+    # three modules being displayed on top of page
+	top_books = Book.objects.order_by('-year_published')[:5]
+	new_books = Book.objects.order_by('-year_published')[:5]
+	recommended_books = Book.objects.filter(genre=0).order_by('year_published')
 
     # books for the genre selector
-    genre = request.GET.get('genre') if request.GET.get('genre') is not None else 0
-    genre_books = Book.objects.filter(genre=genre).order_by('-year_published')[:5]
-    return render(request, 'books/homepage/index.html', {'top_books': top_books, 'new_books': new_books, 'book_by_genre' : genre_books, 'genres' : genres, 'genre': genres[int(genre)]})
+	genre = request.GET.get('genre') if request.GET.get('genre') is not None else 0
+	genre_books = Book.objects.filter(genre=genre).order_by('-year_published')[:5]
+	return render(request, 'books/homepage/index.html', {'top_books': top_books, 'new_books': new_books, 'book_by_genre' : genre_books, 'genres' : genres, 'genre': genres[int(genre)], 'recommended_books':recommended_books})
 
