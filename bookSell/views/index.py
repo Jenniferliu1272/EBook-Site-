bacad8a3749474fb55f8880dc9c81de22068cda8
Ram Longman <Ram Longman>
2016-11-25 17:57:24 -0500

    basic recommendation

--- bookSell/views/index.py
+++ bookSell/views/index.py
@@ -2,14 +2,20 @@ from django.conf.urls import include, url
 from django.shortcuts import render,get_object_or_404,render_to_response,redirect
 from django import template
 
-from bookSell.models import Book
+from bookSell.models import Book, UserProfile
+from django.contrib.auth.models import User
 from bookSell.constants import genres
 
 def index(request):
     # three modules being displayed on top of page
 	top_books = Book.objects.order_by('-year_published')[:5]
 	new_books = Book.objects.order_by('-year_published')[:5]
-	recommended_books = Book.objects.filter(genre=0).order_by('year_published')
+	
+	if request.user.is_authenticated:
+		fgenre = request.user.userprofile.favorite_genre
+		recommended_books = Book.objects.filter(genre=fgenre).order_by('year_published')
+	else:
+		recommended_books = None
 
     # books for the genre selector
 	genre = request.GET.get('genre') if request.GET.get('genre') is not None else 0
